c5dd87a6c293ad1bf1b5559abdbc085a936324db
Graham Woolley <gwoolley@berkeley.edu>
2022-10-03 15:07:15 -0700

    Updates

---
+++ precache-manifest.cd504bfb1e8af3c95f6b27c85467be9e.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9418f842b44a16cfcc4",
+    "url": "/myapp/static/css/main.8e2849fb.chunk.css"
+  },
+  {
+    "revision": "e9418f842b44a16cfcc4",
+    "url": "/myapp/static/js/main.e9418f84.chunk.js"
+  },
+  {
+    "revision": "aa1ef92c5cf7728f9d0a",
+    "url": "/myapp/static/js/1.aa1ef92c.chunk.js"
+  },
+  {
+    "revision": "6fe01c5ba771801b5d34",
+    "url": "/myapp/static/js/2.6fe01c5b.chunk.js"
+  },
+  {
+    "revision": "48469ed31e21cff15d2b",
+    "url": "/myapp/static/js/runtime~main.48469ed3.js"
+  },
+  {
+    "revision": "3f3d5fd716addfbc8d4fbfd20a0fbb35",
+    "url": "/myapp/static/media/g_j-grad.3f3d5fd7.jpg"
+  },
+  {
+    "revision": "bbf9183059e322a4786b7993be67ef88",
+    "url": "/myapp/static/media/atom-trapping.bbf91830.png"
+  },
+  {
+    "revision": "15dd091581b109c2a09cbe85c4475570",
+    "url": "/myapp/static/media/avatar.15dd0915.png"
+  },
+  {
+    "revision": "e74cdc94bbe44c8989b9ad90c46aa515",
+    "url": "/myapp/static/media/bayviewcafe.e74cdc94.jpg"
+  },
+  {
+    "revision": "4ac722d84ce67b7f6d878505c422b42b",
+    "url": "/myapp/static/media/directory.4ac722d8.png"
+  },
+  {
+    "revision": "1289ef04239b5dcf8916b703a7dce8b9",
+    "url": "/myapp/static/media/sass.1289ef04.png"
+  },
+  {
+    "revision": "bee4565959ef728000cf2b158fa61508",
+    "url": "/myapp/static/media/darkmatter.bee45659.jpg"
+  },
+  {
+    "revision": "8da441fc507d6d140162684ab1b02478",
+    "url": "/myapp/static/media/geochron.8da441fc.jpg"
+  },
+  {
+    "revision": "8cd50edf8a3a4ce26741af31de6b4134",
+    "url": "/myapp/static/media/earth.8cd50edf.gif"
+  },
+  {
+    "revision": "0b5fd60995145ef6ebdf3830a8961d6d",
+    "url": "/myapp/static/media/jenga-smort.0b5fd609.jpg"
+  },
+  {
+    "revision": "a03af60075b8124edf13810f267eb927",
+    "url": "/myapp/static/media/github.a03af600.svg"
+  },
+  {
+    "revision": "e4db07409c938ee94a66b7a37962aff4",
+    "url": "/myapp/static/media/linkedin.e4db0740.svg"
+  },
+  {
+    "revision": "3e0a3b540f57a37c2194242da84d2f5a",
+    "url": "/myapp/static/media/twitter.3e0a3b54.svg"
+  },
+  {
+    "revision": "75bec2f0d5a5c8d7973fbc99cba1dabf",
+    "url": "/myapp/index.html"
+  }
+];
