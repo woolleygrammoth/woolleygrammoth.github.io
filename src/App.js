0a6470059e12fafb48d49076db479d13929c0995
Graham Woolley <gwoolley@berkeley.edu>
2022-10-03 16:48:15 -0700

    router troubleshooting

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import './css/styles.css'
 
 function App() {
   return (
-    <Router>
+    <Router basename={`${process.env.PUBLIC_URL}`}>
       <Header />
       <Routes>
         <Route path='' element={<Home />} />
